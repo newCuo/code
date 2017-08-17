0989722868fe628553de98d526c98311aba49979
lirch <573475446@qq.com>
2017-08-17 17:49:47 +0800

    小修改

--- NewCuo/Class/Goods/A_GoodsDetail/ViewModel/GoodsDetailBottomViewModel.m
+++ NewCuo/Class/Goods/A_GoodsDetail/ViewModel/GoodsDetailBottomViewModel.m
@@ -10,20 +10,28 @@
 #import "GoodsDetailBottomView.h"
 #import "GoodsDetailView.h"
 
+
 @interface GoodsDetailBottomViewModel()
 
-
-
+@property (strong,nonatomic)GoodsDetailBottomView *bottomView;
 
 @end
 @implementation GoodsDetailBottomViewModel
 
 
 -(void)initUI{
-    GoodsDetailBottomView *bottomView = [[GoodsDetailBottomView alloc]initWithFrame:CGRectMake(0, KSCREENHEIGHT - 60*SCALE, KSCREENWIDTH, 60*SCALE)];
-    [self.LB_View addSubview:bottomView];
+    self.bottomView = [[GoodsDetailBottomView alloc]initWithFrame:CGRectMake(0, KSCREENHEIGHT - 60*SCALE, KSCREENWIDTH, 60*SCALE)];
+    [self.LB_View addSubview:self.bottomView];
 }
--(void)bindingEvent{
-    
+-(void)bindingEvent{    
+    @weakify(self);
+    [[self.bottomView.addToShoppingCartBtn rac_signalForControlEvents:(UIControlEventTouchUpInside)] subscribeNext:^(id x) {
+        @strongify(self);
+//        LI_GoodsSpecificationSelectVC *goodsSpecificationSelectVC = [LI_GoodsSpecificationSelectVC new];
+//        goodsSpecificationSelectVC.modalPresentationStyle = UIModalPresentationOverFullScreen;//设置样式，与背景透明配合
+//        [self.viewController.navigationController presentViewController:goodsSpecificationSelectVC animated:NO completion:^{
+//            goodsSpecificationSelectVC.view.superview.backgroundColor = [UIColor clearColor];//设置背景为透明色
+//        }];
+    }];
 }
 @end
